3dbbe051b6df890c4f8cc8da8a1c2e8547bcba6d
bodempudi-nalini <89367369+bodempudi-nalini@users.noreply.github.com>
2021-11-16 11:06:00 +0530

    Update Startup.cs

--- na-vs2019-pullRequestDemo/Startup.cs
+++ na-vs2019-pullRequestDemo/Startup.cs
@@ -32,7 +32,7 @@ namespace na_vs2019_pullRequestDemo
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello NALINI!");
                 });
             });
         }
